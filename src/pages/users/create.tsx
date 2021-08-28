9ac8c07028d63f9cbc88c75386bd02340bf90ee9
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2021-08-27 21:51:55 -0300

    chore: Data fetching e cache local

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,45 +1,78 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
-import { SubmitHandler, useForm } from 'react-hook-form'
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
-import { yupResolver } from '@hookform/resolvers/yup'
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import Link from "next/link";
+
+import { useMutation } from "react-query";
 
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
-
-type CreateUserFormData={
+type CreateUserFormData = {
   name: string;
   email: string;
   password: string;
   password_confirmation: string;
-
-
 };
 
 const createUserFormSchema = yup.object().shape({
-  name: yup.string().required('Nome Obrigatório'),
-  email: yup.string().required('E-mail Obrigatório').email('E-mail inválido'),
-  password: yup.string().required('Senha obrigatória').min(6, 'Minimo de 6 caracteres'),
-  password_confirmation: yup.string().oneOf([null,yup.ref('password')], 'As senhas precisam ser iguais') 
-
-})
-
+  name: yup.string().required("Nome Obrigatório"),
+  email: yup.string().required("E-mail Obrigatório").email("E-mail inválido"),
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "Minimo de 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
+});
 
 export default function CreateUser() {
 
-  const { register, handleSubmit, formState} = useForm({
-    resolver: yupResolver(createUserFormSchema)
-  })
+  const router = useRouter();
 
-  const {errors} = formState;
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) =>{
-    await new Promise(resolve => setTimeout(resolve,2000));
-    console.log(values)
-  }
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post("users", {
+      user: {
+        ...user,
+        created_at: new Date(),
+      },
+    });
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+    }
+  });
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+
+  const { errors } = formState;
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await  createUser.mutateAsync(values);
+
+    router.push('/users')
+  };
 
   return (
     <Box>
@@ -47,70 +80,70 @@ export default function CreateUser() {
       <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-      <Box
+        <Box
           as="form"
           flex="1"
           borderRadius={8}
           bg="gray.800"
-          p={["6","8"]}
+          p={["6", "8"]}
           onSubmit={handleSubmit(handleCreateUser)}
         >
-            <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
-            <Divider my="6" boderColor="gray.700"/> 
+          <Heading size="lg" fontWeight="normal">
+            Criar Usuário
+          </Heading>
+          <Divider my="6" boderColor="gray.700" />
 
-            <VStack spacing="8">
-                <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
-                    <Input
-                      name="name"
-                      label="Nome completo"
-                      error={errors.name}
-                      {...register('name')}
-                    />
-                    <Input
-                      name="email"
-                      type="email"
-                      label="E-mail"
-                      error={errors.email}
-                      {...register('email')}
-                    />
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="name"
+                label="Nome completo"
+                error={errors.name}
+                {...register("name")}
+              />
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                error={errors.email}
+                {...register("email")}
+              />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
+                error={errors.password}
+                {...register("password")}
+              />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha"
+                error={errors.password_confirmation}
+                {...register("password_confirmation")}
+              />
+            </SimpleGrid>
+          </VStack>
 
-                </SimpleGrid>
-                <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
-                    <Input
-                      name="password"
-                      type="password"
-                      label="Senha"
-                      error={errors.password}
-                      {...register('password')}
-                    />
-                    <Input
-                      name="password_confirmation"
-                      type="password"
-                      label="Confirmação da senha"
-                      error={errors.password_confirmation}
-                      {...register('password_confirmation')}
-                    />
-                </SimpleGrid>
-            </VStack>
-
-            <Flex mt="8" justify="flex-end">
-                <HStack spacing="4">
-                <Link href="/users" passHref>
-                    <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
-                    </Link>
-                    <Button
-                      type="submit"
-                      colorScheme="pink"
-                      isLoading={formState.isSubmitting}
-                      >
-                        Salvar
-                        </Button>
-
-                </HStack>
-
-            </Flex>
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
         </Box>
-
       </Flex>
     </Box>
   );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,28 +14,40 @@ import {
   Text,
   useBreakpointValue,
   Spinner,
+  Link,
 } from "@chakra-ui/react";
-import Link from "next/link";
+import { GetServerSideProps } from "next";
+import NextLink from "next/link";
 import { useState } from "react";
 
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
  
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers} from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
-export default function UserList() {
+export default function UserList({users}) {
   const [page , setPage] = useState(1);
   const { data, isLoading, isFetching, error } =  useUsers(page);
-
-  console.log(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(["user", userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10, // 10 minutes
+    })
+  }
 
   return (
     <Box>
@@ -52,7 +64,7 @@ export default function UserList() {
               )}
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -62,7 +74,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -95,7 +107,9 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
+                            <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
                             <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">
                               {user.email}
                             </Text>
@@ -121,3 +135,13 @@ export default function UserList() {
     </Box>
   );
 }
+
+// export const getServerSideProps: GetServerSideProps = async() => {
+//   const {users, totalCount} = await getUsers(1);
+//   return { 
+//     props: {
+//       users,
+
+//     }
+//   }
+// }
